097d60ecffa4368d6bb5d109ad9b025bb119264d
ATXUP <atx6me@peernat.com>
2025-11-24 06:54:44 -0800

    Update [[default]].js

--- node-functions/express/[[default]].js
+++ node-functions/express/[[default]].js
@@ -3,14 +3,8 @@ const app = express();
 
 // 添加日志中间件
 app.use((req, res, next) => {
-  // console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
-  // next();
-  let ip=/\d+\.\d+\.\d+\.\d+/.exec(req.url)
-  if(!ip)ip=/[\w:]{8,}/.exec(req.url)
-  if(ip)ip=ip[0]
-  else ip=req.ip
-  let d=await ipinfo(ip)
-  res.json({ message: "Hello from Express on Node Functions!",ip,d ,headers:req.headers});
+  console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+  next();
 });
 async function ipinfo(ip) {
     let d = await fetch(`https://ipinfo.io/${ip||''}/json?token=c4d56ffce02403`)
@@ -18,7 +12,12 @@ async function ipinfo(ip) {
 }
 // 添加根路由处理
 app.get("/",async (req, res) => {
-  res.json({ message: "Functions!");
+  let ip=/\d+\.\d+\.\d+\.\d+/.exec(req.url)
+  if(!ip)ip=/[\w:]{8,}/.exec(req.url)
+  if(ip)ip=ip[0]
+  else ip=req.ip
+  let d=await ipinfo(ip)
+  res.json({ message: "Hello from Express on Node Functions!",ip,d ,headers:req.headers});
 });
 
 // 导出处理函数
