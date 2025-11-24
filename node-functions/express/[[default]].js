c6899a997e9368c4e5a3697a65c7b729ec58287a
ATXUP <atx6me@peernat.com>
2025-11-24 06:11:29 -0800

    Update [[default]].js

--- node-functions/express/[[default]].js
+++ node-functions/express/[[default]].js
@@ -6,10 +6,14 @@ app.use((req, res, next) => {
   console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
   next();
 });
-
+async function ipinfo(ip) {
+    let d = await fetch(`https://ipinfo.io/${ip||''}/json`)
+    return await d.json()
+}
 // 添加根路由处理
-app.get("/", (req, res) => {
-  res.json({ message: "Hello from Express on Node Functions!" });
+app.get("/",async (req, res) => {
+  let d=await ipinfo()
+  res.json({ message: "Hello from Express on Node Functions!",d ,eo:req.eo});
 });
 
 // 导出处理函数
